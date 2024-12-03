9208d7c1b575371f39a1d3f293076970e656a571
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-12-03 12:35:36 +0500

    Added font and Color scheme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodyColor: '#EAE1D1',
+        'text-color': '#FFFFFF',
+        'btn-yellow': '#E1FF00',
+        'content-bg': '#6899E1',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        judson: ['Judson', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
